d2c23af5718ef6254189f7debddb85678b902960
esimkus20 <ernestas.simk@gmail.com>
2023-09-27 01:23:25 +0300

    Responsive + Interactive + Javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var firstName = document.getElementById("firstName")
+var surname = document.getElementById("surname")
+var number = document.getElementById("number")
+var monthly = document.getElementById("monthly")
+var yearly = document.getElementById("yearly")
+var submit = document.getElementById("submit")
+
+// if (vardas)
