ef1e56335a4c2375c1caf4982638ef5f2d1cb9c6
esimkus20 <ernestas.simk@gmail.com>
2023-09-28 16:17:14 +0300

    sound fix

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ AOS.init();
 const sound1 = document.getElementById("sound-1")
 const sound2 = document.getElementById("sound-2")
 const sound3 = document.getElementById("sound-3")
-const customSound1 = new Audio("../app/sounds/sound-1.wav")
-const customSound2 = new Audio("../app/sounds/sound-2.wav")
-const customSound3 = new Audio("../app/sounds/sound-3.wav")
+const customSound1 = new Audio("../app/sounds/sound-1.mp3")
+const customSound2 = new Audio("../app/sounds/sound-2.mp3")
+const customSound3 = new Audio("../app/sounds/sound-3.mp3")
 
 sound1.addEventListener("click", function() {
   customSound1.play()
